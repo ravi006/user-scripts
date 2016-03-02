3cdbf7c1a7287dda56c16ed6ce840da20acf6a71
ravi <ravi@ravi.(none)>
2016-03-03 00:10:41 +0530

    Adds a script file for create a user

---
+++ user/create/create_user_in_different_directory.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+
+sudo groupadd tapela
+
+
+sudo adduser --home /tapela/ravi ravi
+
+sudo chown prcsrvce:prcsrvce /tapela/ravi
+
+sudo chmod 755 /tapela/ravi
+
+
+sudo cat /etc/passwd | grep ravi
+
+#add user in group
+
+sudo usermod -a -G tapela ravi
+
+sudo chown ravi:tapela /appl/ravi
+
+#change primary group of priceservice from priceservice to appl
+sudo usermod -g newgroup username
+sudo usermod -g tapela ravi
+
+# delete secondary group
+sudo groupdel ravi
+
+#if user not sudo group, then add
+
+sudo gpasswd -a username sudo
+
+
+#ssh
+ssh-keygen -t rsa
+
+sudo apt-get install openssh-server
